6ee8826e32c9a20e4ba5faf60209c7ed358cdd9d
dx83 <dxorder@gmail.com>
2026-04-23 20:42:14 +0900

    study 260423

--- src/App.js
+++ src/App.js
@@ -20,10 +20,24 @@ const App = () => {
       //}
       //document.write(`<h1>어서오세요!</h1>
       //  <p>자바스크립트로 작성된 웹 페이지입니다.</p>`);
-      let name = prompt("이름을 입력하세요.");
+      //let name = prompt("이름을 입력하세요.");
       //document.write("<b><big>" + name + "</big></b>님, 환영합니다!<br />");
-      document.write(`<b><big>${name}</big></b>님, 환영합니다!`);
+      //document.write(`<b><big>${name}</big></b>님, 환영합니다!`);
       //console.log(`${name}님, 환영합니다!`);
+
+      //let width;
+      //let height;
+      //width = 200;
+      //height = 50;
+      //let area = width * height;
+      //console.log(`넓이는 ${area}입니다.`);
+
+      const PI = 3.14;
+      let radius = prompt("원의 반지름을 입력하세요.");
+      let area = PI * radius * radius;
+      console.log(`${PI}원의 넓이는 ${area}입니다.`);
+
+
     }
 
 
